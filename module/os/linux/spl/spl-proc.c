31aedb9df116d4ac0de23316c81bd0adabd0f976
Serapheim Dimitropoulos <serapheimd@gmail.com>
2020-05-01 21:43:57 -0700

    Revert "Update skc_obj_alloc for spl kmem caches that are backed by Linux (#138)" (#161)
    This reverts commit f75dea29b3ac9a4d63017e6c8fc41b4896a8bc22.
    
    We've hit a performance regression in this commit where running the
    ZFS performance suite we've observed 1~8% slowdown. As the commit
    was made purely for debugging purposes in a code path that doesn't
    change often, the revert is pretty low-risk.

--- module/os/linux/spl/spl-proc.c
+++ module/os/linux/spl/spl-proc.c
@@ -437,29 +437,11 @@ slab_seq_show(struct seq_file *f, void *p)
 
 	ASSERT(skc->skc_magic == SKC_MAGIC);
 
-	if (skc->skc_flags & KMC_SLAB) {
-		/*
-		 * This cache is backed by a generic Linux kmem cache which
-		 * has its own accounting. For these caches we only track
-		 * the number of active allocated objects that exist within
-		 * the underlying Linux slabs. For the overall statistics of
-		 * the underlying Linux cache please refer to /proc/slabinfo.
-		 */
-		spin_lock(&skc->skc_lock);
-		seq_printf(f, "%-36s  ", skc->skc_name);
-		seq_printf(f, "0x%05lx %9s %9lu %8s %8u  "
-		    "%5s %5s %5s  %5s %5lu %5s  %5s %5s %5s\n",
-		    (long unsigned)skc->skc_flags,
-		    "-",
-		    (long unsigned)(skc->skc_obj_size * skc->skc_obj_alloc),
-		    "-",
-		    (unsigned)skc->skc_obj_size,
-		    "-", "-", "-", "-",
-		    (long unsigned)skc->skc_obj_alloc,
-		    "-", "-", "-", "-");
-		spin_unlock(&skc->skc_lock);
+	/*
+	 * Backed by Linux slab see /proc/slabinfo.
+	 */
+	if (skc->skc_flags & KMC_SLAB)
 		return (0);
-	}
 
 	spin_lock(&skc->skc_lock);
 	seq_printf(f, "%-36s  ", skc->skc_name);
@@ -479,7 +461,9 @@ slab_seq_show(struct seq_file *f, void *p)
 	    (long unsigned)skc->skc_obj_deadlock,
 	    (long unsigned)skc->skc_obj_emergency,
 	    (long unsigned)skc->skc_obj_emergency_max);
+
 	spin_unlock(&skc->skc_lock);
+
 	return (0);
 }
 
