72adb5b74e77906b9402d2e46c7787982191d969
Matthew Ahrens <mahrens@delphix.com>
2021-05-12 20:56:12 -0700

    logging timezone

--- cmd/zfs_object_agent/server/src/main.rs
+++ cmd/zfs_object_agent/server/src/main.rs
@@ -73,43 +73,33 @@ fn main() {
         matches.value_of("output-file"),
     );
 
-    // error!() should be used when an invalid state is encountered; the related
-    // operation will fail and the program may exit.  E.g. an invalid request
-    // was received from the client (kernel).
     error!(
-        "logging level ERROR enabled; local timezone is {}",
+        "Starting ZFS Object Agent.  Local timezone is {}",
         chrono::Local::now().format("%Z (%:z)")
     );
 
+    // error!() should be used when an invalid state is encountered; the related
+    // operation will fail and the program may exit.  E.g. an invalid request
+    // was received from the client (kernel).
+    error!("logging level ERROR enabled");
+
     // warn!() should be used when something unexpected has happened, but it can
     // be recovered from.
-    warn!(
-        "logging level WARN enabled; local timezone is {}",
-        chrono::Local::now().format("%Z (%:z)")
-    );
+    warn!("logging level WARN enabled");
 
     // info!() should be used for very high level operations which are expected
     // to happen infrequently (no more than once per minute in typical
     // operation).  e.g. opening/closing a pool, long-lived background tasks,
     // things that might be in `zpool history -i`.
-    info!(
-        "logging level INFO enabled; local timezone is {}",
-        chrono::Local::now().format("%Z (%:z)")
-    );
+    info!("logging level INFO enabled");
 
     // debug!() can be used for all but the most frequent operations.
     // e.g. not every single read/write/free operation, but perhaps for every
     // call to S3.
-    debug!(
-        "logging level DEBUG enabled; local timezone is {}",
-        chrono::Local::now().format("%Z (%:z)")
-    );
+    debug!("logging level DEBUG enabled");
 
     // trace!() can be used indiscriminately.
-    trace!(
-        "logging level TRACE enabled; local timezone is {}",
-        chrono::Local::now().format("%Z (%:z)")
-    );
+    trace!("logging level TRACE enabled");
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
